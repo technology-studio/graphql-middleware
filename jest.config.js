59b6b74234cec6ee645bd645cd9119d6c244b5c1
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2022-11-03 09:10:13 +0100

    chore: fix axios import in unit-tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/tests', '<rootDir>/src'],
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   testPathIgnorePatterns: ['/node_modules/', '/__fixtures__/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
